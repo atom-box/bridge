8d1c275fa99310195649de9d10fb87f550d0e947
atom-box <genest.1@osu.edu>
2019-03-07 09:22:06 -0600

    Fixed toString.

--- mode.elm
+++ mode.elm
@@ -2,12 +2,15 @@ import Browser
 import Html exposing (Html, button, div, text)
 import Html.Events exposing (onClick)
 
+triples: List Int
+triples = [1, 1, 4, 0, 4, 2, 5, 2, 0, 1, 2, 1, 3, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+strTriples = toString triples
 
 
 view model =
   div []
     [ button [ ] [ text "-" ]
-    , div [] [ text "here here hear" ]
+    , div [] [ text strTriples ]
     , button [  ] [ text "+" ]
     ]
 
