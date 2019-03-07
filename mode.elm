ab3159a2e3acb94321566237fa04529e0a994d1b
Evan Genest <genest.1@osu.edu>
2019-03-07 07:50:47 -0600

    Should really stash.  Restore pt before Dundermifflin Poudel.

--- mode.elm
+++ mode.elm
@@ -1,3 +1,25 @@
-type alias Model = (List, Int)
+import Browser
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onInput)
+
+
+
+-- MAIN
+
+
+main =
+  Browser.sandbox { init = x,  view = view }
+
+type alias Model = (List Int)
 x: Model
 x = [77,78,81,85,90]
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+  div []
+    [ text x ]
+
+
